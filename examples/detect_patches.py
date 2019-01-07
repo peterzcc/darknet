bd4350fc8fe689e2a811a0a027d44b3fb23e11e7
peter <peterzengcc@gmail.com>
2019-01-07 17:55:57 +0800

    changed to multi processing

--- examples/detect_patches.py
+++ examples/detect_patches.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 import time
 import argparse
 import os
+import multiprocessing as mp
+import queue
+
 
 class DetectedObject(object):
     def __init__(self, name, prob, x, y, w, h):
@@ -59,15 +62,15 @@ def pad_img_to_fit_bbox(img, x1, x2, y1, y2):
     return img, x1, x2, y1, y2
 
 
-def resize_image(img, target_width=1920):
+def resize_image(img, target_width=3840):
     im = cv2.resize(img, dsize=(target_width, int(img.shape[0] * target_width / img.shape[1])),
                     interpolation=cv2.INTER_NEAREST)
     return im
 
 
 def generate_patches_from_image(im_ori, patch_width=608,
-                                offsets=np.array(((-1,0), (0,0), (1,0))),
-                                default_offset=np.array([0,0],dtype=np.int)):
+                                offsets=np.array(((-2, 0), (-1, 0), (0, 0), (1, 0), (2, 0))),
+                                default_offset=np.array([-392, 360], dtype=np.int)):
     center = np.array([im_ori.shape[1]/2, im_ori.shape[0]/2], dtype=np.int)+default_offset
     pw = patch_width
     real_offsets = np.tile(offsets * pw, (1,2))
@@ -97,6 +100,7 @@ def _detector(net, meta, image, thresh=.5, hier=.5, nms=.45):
     dn.free_detections(dets, num_det)
     return res
 
+
 def draw_detections(im_ori, results, patch_boxes, PERSON_LABEL=0):
     im_proc = im_ori.copy()
     num_pred = 0
@@ -120,9 +124,8 @@ def draw_detections(im_ori, results, patch_boxes, PERSON_LABEL=0):
                 cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 5)
     return im_proc, num_pred
 
+
 def detect_patches(im_ori, patch_boxes, net, meta):
-
-
     results = []
     start_time = time.time()
     for i, box in enumerate(patch_boxes):
@@ -139,13 +142,13 @@ def detect_patches(im_ori, patch_boxes, net, meta):
     return results
 
 
-def run_test_image(net, meta, patch_boxes, show=0, PERSON_LABEL=0):
+def run_test_image(net, meta, show=0, PERSON_LABEL=0):
     im_ori = cv2.imread("images/test.jpg")
     im_ori = resize_image(im_ori)
+    patch_boxes = None
     if patch_boxes is None:
         patch_boxes = generate_patches_from_image(
-            im_ori,
-            default_offset=np.array([-45, 120], dtype=np.int))
+            im_ori)
 
     det_results = detect_patches(im_ori, patch_boxes, net, meta)
     im_proc, num_pred = draw_detections(im_ori, det_results, patch_boxes, PERSON_LABEL)
@@ -158,6 +161,77 @@ def run_test_image(net, meta, patch_boxes, show=0, PERSON_LABEL=0):
         cv2.imwrite("outputs/output.jpg", im_proc)
 
 
+def run_video_detection(args, datadir,outputdir, video_name, gpu_id=0):
+    dn.cuda_set_device(gpu_id)
+    net = dn.load_network("cfg/{}.cfg".format(args.model), "models/{}.weights".format(args.model), 0)
+    meta = dn.get_metadata("cfg/coco.data")
+    PERSON_LABEL = None
+    for i in range(meta.classes):
+        if meta.names[i] == b"person":
+            PERSON_LABEL = i
+    patch_boxes = None
+    print("Processing video: {}".format(video_name))
+    out_frames_path = os.path.join(outputdir, video_name)
+    assert not os.path.isdir(out_frames_path)
+    os.mkdir(out_frames_path)
+    in_frames_path = os.path.join(datadir, video_name)
+    det_results = None
+    for i, frame_name in enumerate(os.listdir(in_frames_path)):
+        in_img_path = os.path.join(in_frames_path, frame_name)
+        out_img_path = os.path.join(out_frames_path, frame_name)
+
+        im_ori = cv2.imread(in_img_path)
+        im_ori = resize_image(im_ori)
+        if patch_boxes is None:
+            patch_boxes = generate_patches_from_image(
+                im_ori)
+        if det_results is None or i % args.fskip == 0:
+            det_results = detect_patches(im_ori, patch_boxes, net, meta)
+        im_proc, num_pred = draw_detections(im_ori, det_results, patch_boxes, PERSON_LABEL)
+        if i % 15 == 0:
+            print("outputing: {}".format(out_img_path))
+        cv2.imwrite(out_img_path, im_proc)
+
+
+def run_full_detections(args):
+    outputdir = args.outputdir
+    if os.path.exists(outputdir):
+        os.rmdir(outputdir)
+    os.mkdir(outputdir)
+    datadir = args.datadir
+    assert os.path.isdir(datadir)
+    processes = []
+    queues = []
+    for gpu_id in args.gpu_ids:
+        this_queue = mp.Queue()
+        queues.append(this_queue)
+        this_process = mp.Process(
+            target=detection_process,
+            args=(args, datadir, outputdir, gpu_id, this_queue))
+        processes.append(this_process)
+        this_process.start()
+    i = 0
+    for video_name in os.listdir(datadir):
+        queues[i].put(video_name)
+        i = (i + 1) % len(queues)
+
+    for process in processes:
+        process.join()
+
+
+def detection_process(args, datadir, outputdir, gpu_id, q: mp.Queue):
+    finished = False
+    while not finished:
+        try:
+            video_name = q.get(block=True, timeout=10.0)
+            if video_name is not None:
+                run_video_detection(args, datadir, outputdir, video_name, gpu_id)
+            else:
+                finished = True
+        except queue.Empty:
+            print("empty que")
+        else:
+            print("uncaght exception")
 
 
 def main():
@@ -166,49 +240,26 @@ def main():
     parser.add_argument('--model', type=str, default="yolov3-tiny", help='model name')
     parser.add_argument('--show', type=int, default=0, help='whether to show image')
     parser.add_argument('--fstart', type=int, default=0, help='')
-    parser.add_argument('--fend', type=int, default=-1, help='e')
+    parser.add_argument('--fend', type=int, default=-1, help='')
     parser.add_argument('--fskip', type=int, default=1, help='')
     parser.add_argument('--datadir', type=str, default="frames_southgate", help='')
     parser.add_argument('--outputdir', type=str, default="frames_output", help='')
+    parser.add_argument('--test', type=int, default=1, help='')
     args = parser.parse_args()
-    dn.cuda_set_device(0)
-    net = dn.load_network("cfg/{}.cfg".format(args.model), "models/{}.weights".format(args.model), 0)
-    meta = dn.get_metadata("cfg/coco.data")
-    PERSON_LABEL = None
-    for i in range(meta.classes):
-        if meta.names[i] == b"person":
-            PERSON_LABEL = i
-    patch_boxes = None
+    args.gpu_ids = [0, 1, 2, 3]
 
-    outputdir = args.outputdir
-    if os.path.exists(outputdir):
-        os.rmdir(outputdir)
-    os.mkdir(outputdir)
+    if args.test == 1:
+        dn.cuda_set_device(0)
+        net = dn.load_network("cfg/{}.cfg".format(args.model), "models/{}.weights".format(args.model), 0)
+        meta = dn.get_metadata("cfg/coco.data")
+        PERSON_LABEL = None
+        for i in range(meta.classes):
+            if meta.names[i] == b"person":
+                PERSON_LABEL = i
 
-    datadir = args.datadir
-    assert os.path.isdir(datadir)
-    for video_name in os.listdir(datadir):
-        print("Processing video: {}".format(video_name))
-        out_frames_path = os.path.join(outputdir, video_name)
-        assert not os.path.isdir(out_frames_path)
-        os.mkdir(out_frames_path)
-        in_frames_path = os.path.join(datadir, video_name)
-        det_results = None
-        for i, frame_name in enumerate(os.listdir(in_frames_path)):
-            in_img_path = os.path.join(in_frames_path, frame_name)
-            out_img_path = os.path.join(out_frames_path, frame_name)
-
-            im_ori = cv2.imread(in_img_path)
-            im_ori = resize_image(im_ori)
-            if patch_boxes is None:
-                patch_boxes = generate_patches_from_image(
-                    im_ori,
-                    default_offset=np.array([-45, 120], dtype=np.int))
-            if det_results is None or i % args.fskip == 0:
-                det_results = detect_patches(im_ori, patch_boxes, net, meta)
-            im_proc, num_pred = draw_detections(im_ori, det_results, patch_boxes, PERSON_LABEL)
-            print("outputing: {}".format(out_img_path))
-            cv2.imwrite(out_img_path, im_proc)
+        run_test_image(net, meta, args.show, PERSON_LABEL=PERSON_LABEL)
+    else:
+        run_full_detections(args)
 
 
 if __name__ == '__main__':
