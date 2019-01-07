ec7b76f8779bc1e6967937f844e0ca0156471b97
peter <peterzengcc@gmail.com>
2019-01-08 00:16:17 +0800

    changed to multi processing

--- image2video.sh
+++ image2video.sh
@@ -9,5 +9,6 @@ for video_file_name in *.mp4; do
     echo ${video_file_name}
     #mkdir ../${TARGET_DIR}/${video_file_name}
     #ffmpeg -i ${video_file_name} -f image2 ../${TARGET_DIR}/${video_file_name}/f%06d.jpg
-    ffmpeg -framerate ${FRAME_RATE} -i ${video_file_name}/f%06d.jpg -vf "fps=${FRAME_RATE},format=yuv420p" ../${TARGET_DIR}/${video_file_name}
+    ffmpeg -framerate ${FRAME_RATE} -i ${video_file_name}/f%06d.jpg -vf "fps=${FRAME_RATE},format=yuv420p" ../${TARGET_DIR}/${video_file_name} &
 done
+wait
